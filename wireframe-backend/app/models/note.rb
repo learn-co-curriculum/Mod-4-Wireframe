326d017832eee07229c109a60da63fc67420cf41
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-05 18:43:40 -0500

    Add create_with_user and add_tag methods to note model

--- wireframe-backend/app/models/note.rb
+++ wireframe-backend/app/models/note.rb
@@ -2,4 +2,13 @@ class Note < ApplicationRecord
   has_many :tagnotes
   has_many :tags, through: :tagnotes
   belongs_to :user
+
+  def self.create_with_user(title,description,user)
+    user_id = user.id
+    self.create(title: title,description: description, user_id: user_id)
+  end
+
+  def add_tag(tag)
+    TagNote.create(note: self.id ,tag: tag.id)
+  end
 end
