8f6b47682f3f7bb4dec0575d41ee866190382068
GreatDeveloper66 <ashaffe@gmail.com>
2020-05-05 20:21:52 -0500

    Add routes and controllers for users and notes

--- wireframe-backend/app/controllers/welcome_controller.rb
+++ wireframe-backend/app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    render json: Todo.all
+    render json: Note.all
+  end
+  def show
+    note = Note.find_by(id: params[:id])
+    render json: note
   end
 end
